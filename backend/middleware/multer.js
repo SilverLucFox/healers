b2b80f07d1e54dd1d962b2e05a7ce3bd1bc2b318
jawad choucair <120573173+SilverLucFox@users.noreply.github.com>
2024-12-12 23:06:13 +0200

    Delete backend directory

--- backend/middleware/multer.js
+++
@@ -1,8 +0,0 @@
-import multer from "multer";
-const storage = multer.diskStorage({
-  filename: function (req, file, callback) {
-    callback(null, file.originalname);
-  },
-});
-const uplaoad = multer({storage})
-export default uplaoad
--- backend/routes/messagesRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { sendMessage,getMessage } from '../controllers/messagesController.js';
-import { authMessages } from '../middleware/authMessages.js';
-
-const messagesRouter = express.Router();
-
-messagesRouter.post("/send/:id",authMessages,sendMessage)
-messagesRouter.get("/get/:id",authMessages,getMessage)
-export default messagesRouter;
